b6950005bc27c094d5ffffc407bf78820e789445
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-15 00:32:12 -0400

    create spec file for express server

---
+++ server/__test__/server.spec.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../server');
+
+describe('loading express server', () => {
+  test('GET /', async () => {
+    await request(app).get('/').expect(200);
+  });
+
+  test('GET non-existent route', async () => {
+    await request(app).get('/fizz/buzz/bazz').expect(404);
+  });
+});
