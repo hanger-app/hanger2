9caa84b13d827bd94cd3cd77907e212a37da4974
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-15 01:48:28 -0400

    update test script to be in production mode; update server spec to check for html being served

--- server/__test__/server.spec.js
+++ server/__test__/server.spec.js
@@ -3,7 +3,10 @@ const app = require('../server');
 
 describe('loading express server', () => {
   test('GET /', async () => {
-    await request(app).get('/').expect(200);
+    await request(app)
+      .get('/')
+      .expect('Content-Type', /text\/html/)
+      .expect(200);
   });
 
   test('GET non-existent route', async () => {
