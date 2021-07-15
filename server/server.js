6b4330d95bf18b3bbfdfd7c297a3a749c040397d
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-15 00:59:00 -0400

    create production routes for static react assets

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendStatus(200);
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use('/build', express.static(path.resolve(__dirname, '../build')));
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build/index.html'));
+  });
+}
 
 app.use('*', (req, res) => {
   res.sendStatus(404);
