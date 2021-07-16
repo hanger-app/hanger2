042026991a59bc1e7176d9df7b37460135335f60
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-16 02:12:30 -0400

    create connection to a mongodb atlas instance

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
+const mongoConnection = require('./database');
 const app = require('./server');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+mongoConnection
+  .then(() => {
+    console.log('Connected to remote MongoDB instance');
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+  })
+  .catch((error) => console.error(error));
