0bc7cc79191173842f8adfc6da4418f4ad627ff3
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-18 18:49:34 -0400

    create tests for create and read functionality of /api/users

--- server/__test__/server.spec.js
+++ server/__test__/server.spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../server');
+const { setupDatabase, teardownDatabase } = require('./setupTeardownDatabase.js');
 
 describe('loading express server', () => {
   test('GET /', async () => {
@@ -11,5 +12,39 @@ describe('loading express server', () => {
 
   test('GET non-existent route', async () => {
     await request(app).get('/fizz/buzz/bazz').expect(404);
+  });
+});
+
+describe('test CRUD functionality', () => {
+  beforeAll(async () => {
+    try {
+      await setupDatabase();
+      console.log('Connected to remote MongoDB instance');
+    } catch (err) {
+      throw new Error(`ERROR: server.spec.js: ${err}`);
+    }
+  });
+
+  afterAll(async () => {
+    try {
+      await teardownDatabase();
+    } catch (err) {
+      throw new Error(`ERROR: server.spec.js: ${err}`);
+    }
+  });
+
+  test('POST /api/users/fizz@buzz.io', async () => {
+    await request(app)
+      .post('/api/users/fizz@buzz.io')
+      .send({ firstName: 'test_firstName', lastName: 'test_lastName', zipcode: 12345 })
+      .expect('Content-Type', /application\/json/)
+      .expect(200);
+  });
+
+  test('GET /api/users/fizz@buzz.io', async () => {
+    await request(app)
+      .get('/api/users/fizz@buzz.io')
+      .expect('Content-Type', /application\/json/)
+      .expect(200);
   });
 });
