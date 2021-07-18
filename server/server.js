c25a10548334e5d723cb0a4692a174e14b32d0c5
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-18 05:34:12 -0400

    add next to global error handling middleware

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use('/*', (req, res, next) => {
   });
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   const defaultError = {
     log: 'An unexpected middleware error occurred!',
     status: 400,
