e006115467448263420bd904111d04d164f3a4c8
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-20 19:59:15 -0400

    implement oauth login flow with jwt and refresh tokens; will probably need refactor to not store refresh tokens in memory; account creation still needs to be included in flow

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -1,13 +1,11 @@
 const axios = require('axios');
-const {
-  GOOGLE_OAUTH_URL,
-  GOOGLE_OAUTH_BASE_EXCHANGE,
-  GOOGLE_OAUTH_CLIENT_ID,
-  GOOGLE_OAUTH_CLIENT_SECRET,
-  GOOGLE_OAUTH_CALLBACK,
-} = require('../util/googleOAuthConstants.js');
+const jwt = require('jsonwebtoken');
+const { GOOGLE_OAUTH_URL, GOOGLE_OAUTH_EXCHANGE_URL } = require('../util/googleOAuthConstants.js');
+const { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } = require('../util/jwtConstants.js');
 
 const sessionController = {};
+
+const refreshTokens = new Set();
 
 sessionController.login = (req, res, next) => {
   return res.redirect(GOOGLE_OAUTH_URL);
@@ -17,7 +15,7 @@ sessionController.callback = async (req, res, next) => {
   const { code } = req.query;
 
   try {
-    const exchangeUrl = `${GOOGLE_OAUTH_BASE_EXCHANGE}?code=${code}&client_id=${GOOGLE_OAUTH_CLIENT_ID}&client_secret=${GOOGLE_OAUTH_CLIENT_SECRET}&redirect_uri=${GOOGLE_OAUTH_CALLBACK}&grant_type=authorization_code`;
+    const exchangeUrl = `${GOOGLE_OAUTH_EXCHANGE_URL}&code=${code}`;
     const { data } = await axios.post(exchangeUrl);
 
     const userInfo = await axios.get('https://www.googleapis.com/oauth2/v2/userinfo', {
@@ -25,15 +23,116 @@ sessionController.callback = async (req, res, next) => {
     });
 
     res.locals.userInfo = userInfo.data;
+    return next();
+  } catch (err) {
+    console.error(err);
+    return next({
+      log: `ERROR: sessionController.callback: ${err.response}`,
+      status: err.response?.status ?? 500,
+      message: { error: 'There was an error' },
+    });
+  }
+};
+
+sessionController.startSession = async (req, res, next) => {
+  const { email } = res.locals.userInfo;
+
+  try {
+    const accessToken = jwt.sign({ email }, ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+    const refreshToken = jwt.sign({ email }, REFRESH_TOKEN_SECRET);
+
+    refreshTokens.add(refreshToken);
+
+    res.cookie('token', accessToken, { maxAge: 60 * 60 * 24 * 7 * 1000, httpOnly: true });
+    res.cookie('refresh', refreshToken, { maxAge: 60 * 60 * 24 * 7 * 52 * 1000, httpOnly: true });
 
     return next();
   } catch (err) {
     return next({
-      log: `ERROR: sessionController.callback: ${err.response}`,
-      status: err.response.status,
+      log: `ERROR: sessionController.startSession: ${err}`,
+      status: 500,
       message: { error: 'There was an error' },
     });
   }
+};
+
+sessionController.refresh = (req, res, next) => {
+  const { refresh } = req.cookies;
+
+  try {
+    if (!refresh) {
+      throw {
+        log: `ERROR: sessionController.refresh:${new RangeError('No refresh token found in request cookies')}`,
+        status: 401,
+        message: { error: 'Unauthorized' },
+      };
+    }
+
+    if (!refreshTokens.has(refresh)) {
+      throw {
+        log: `ERROR: sessionController.refresh: ${new RangeError('Supplied refresh token does not exist')}`,
+        status: 403,
+        message: { error: 'Forbidden' },
+      };
+    }
+
+    jwt.verify(refresh, REFRESH_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return next({
+          log: `ERROR: sessionController.refresh: jwt.verify: ${err}`,
+          status: 403,
+          message: { error: 'Forbidden' },
+        });
+      }
+
+      const accessToken = jwt.sign({ email: decoded.email }, ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+      res.cookie('token', accessToken, { maxAge: 7 * 60 * 60 * 24 * 1000, httpOnly: true });
+
+      return next();
+    });
+  } catch (err) {
+    return next({
+      log: err.log ?? `ERROR: sessionController.refresh: ${err}`,
+      status: err.status ?? 400,
+      message: err.message ? err.message : { error: 'An error occurred' },
+    });
+  }
+};
+
+sessionController.invalidateRefreshToken = (req, res, next) => {
+  const { refresh } = req.cookies;
+
+  try {
+    if (!refreshTokens.delete(refresh)) {
+      throw {
+        log: `ERROR: sessionController.invalidateRefreshToken: ${new RangeError(
+          'Supplied refresh token does not exist'
+        )}`,
+        status: 204,
+        message: { error: '' },
+      };
+    }
+
+    return next();
+  } catch (err) {
+    return next({
+      log: err.log ?? `ERROR: sessionController.invalidateRefreshToken: ${err}`,
+      status: err.status ?? 400,
+      message: err.message ? err.message : { error: 'An error occurred' },
+    });
+  }
+};
+
+sessionController.createUserCookie = (req, res, next) => {
+  const user = {
+    id: res.locals.userInfo.id,
+    email: res.locals.userInfo.email,
+    google_picture: res.locals.userInfo.picture,
+    authenticated: true,
+  };
+
+  res.cookie('user', user);
+  return next();
 };
 
 module.exports = sessionController;
--- server/routes/sessionRouter.js
+++ server/routes/sessionRouter.js
@@ -5,8 +5,22 @@ const sessionRouter = Router();
 
 sessionRouter.get('/login', sessionController.login);
 
-sessionRouter.get('/callback', sessionController.callback, (req, res) => {
-  return res.status(200).json(res.locals.userInfo);
+sessionRouter.get(
+  '/callback',
+  sessionController.callback,
+  sessionController.startSession,
+  sessionController.createUserCookie,
+  (req, res) => {
+    return res.status(200).json(res.locals.userInfo);
+  }
+);
+
+sessionRouter.post('/refresh', sessionController.refresh, (req, res) => {
+  res.sendStatus(200);
+});
+
+sessionRouter.post('/logout', sessionController.invalidateRefreshToken, (req, res) => {
+  res.sendStatus(204);
 });
 
 module.exports = sessionRouter;
