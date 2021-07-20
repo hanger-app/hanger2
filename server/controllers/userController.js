1c09c3ec2ba479df9b52c6a4b1e5ae95be737c89
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-20 00:23:32 -0400

    rewrite createUser and getUser methods to get email from request body; implement request validation; throw custom error object and return next from catch

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,55 +2,57 @@ const User = require('../models/UserModel.js');
 
 const userController = {
   async createUser(req, res, next) {
-    const { id } = req.params;
-
-    if (!id) {
-      return next({
-        log: 'ERROR: userController.createUser: ID was not provided',
-        status: 400,
-        message: { error: 'Please provide a valid id' },
-      });
-    }
-
     try {
-      const newUser = await User.create({
+      const createUserQuery = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
-        email: id,
+        email: req.body.email,
         zipcode: req.body.zipcode,
-      });
+      };
+
+      for (const [key, value] of Object.entries(createUserQuery)) {
+        if (value === undefined) {
+          throw {
+            log: `ERROR: userController.createUser: ${new TypeError(`${key} is undefined`)}`,
+            status: 400,
+            message: `Please provide ${key}`,
+          };
+        }
+      }
+
+      const newUser = await User.create(createUserQuery);
 
       res.locals.newUser = newUser;
       return next();
     } catch (err) {
       return next({
-        log: `userController.createUser: ${err}`,
-        status: 400,
-        message: { error: 'An error occurred!' },
+        log: err.log ?? `ERROR: userController.createUser: ${err}`,
+        status: err.status ?? 400,
+        message: err.message ? { error: err.message } : { error: 'An error occurred!' },
       });
     }
   },
 
   async getUser(req, res, next) {
-    const { id } = req.params;
-
-    if (!id) {
-      return next({
-        log: 'ERROR: userController.createUser: ID was not provided',
-        status: 400,
-        message: { error: 'Please provide a valid id' },
-      });
-    }
-
     try {
+      const { id } = req.params;
+
+      if (!id) {
+        throw {
+          log: 'ERROR: userController.createUser: ID was not provided',
+          status: 400,
+          message: 'Please provide a valid id',
+        };
+      }
+
       const foundUser = await User.findOne({ email: id });
 
       if (!foundUser) {
-        return next({
+        throw {
           log: 'ERROR: userController.getUser: ID does not exist',
           status: 404,
-          message: { error: 'The provided email does not exist.' },
-        });
+          message: 'The provided email does not exist.',
+        };
       }
 
       res.locals.foundUser = foundUser;
@@ -58,9 +60,9 @@ const userController = {
       return next();
     } catch (err) {
       return next({
-        log: `userController.getUser: ${err}`,
-        status: 400,
-        message: { error: 'An error occurred!' },
+        log: err.log ?? `ERROR: userController.getUser: ${err}`,
+        status: err.status ?? 400,
+        message: err.message ? { error: err.message } : { error: 'An error occurred!' },
       });
     }
   },
