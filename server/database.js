0d705db83fb892a49e59bb69a9f8d285122bc95d
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-21 20:17:33 -0400

    Load models before connecting to MongoDB

--- server/database.js
+++ server/database.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+// Load models before connecting to MongoDB
+require('./models/UserModel.js');
+require('./models/ClothingModel.js');
+
 const mongoConnection = mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
