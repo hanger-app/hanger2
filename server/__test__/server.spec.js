fbb553a4ac3b85d098f206d8cdc825b2c74668b3
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-21 23:39:26 -0400

    remove POST /api/users route; spy on global.console in "GET non-existent route"

--- server/__test__/server.spec.js
+++ server/__test__/server.spec.js
@@ -11,7 +11,12 @@ describe('loading express server', () => {
   });
 
   test('GET non-existent route', async () => {
+    const errorSpy = jest.spyOn(global.console, 'error').mockImplementation(() => jest.fn());
+
     await request(app).get('/fizz/buzz/bazz').expect(404);
+    expect(errorSpy).toHaveBeenCalledTimes(1);
+
+    errorSpy.mockRestore();
   });
 });
 
@@ -31,20 +36,6 @@ describe('test CRUD functionality', () => {
     } catch (err) {
       throw new Error(`ERROR: server.spec.js: ${err}`);
     }
-  });
-
-  xtest('POST /api/users', async () => {
-    await request(app)
-      .post('/api/users')
-      .send({ firstName: 'test_firstName', lastName: 'test_lastName', email: 'fizz@buzz.io', zipcode: 12345 })
-      .expect('Content-Type', /application\/json/)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body.firstName).toEqual('test_firstName');
-        expect(res.body.lastName).toEqual('test_lastName');
-        expect(res.body.email).toEqual('fizz@buzz.io');
-        expect(res.body.zipcode).toEqual(12345);
-      });
   });
 
   xtest('GET /api/users/fizz@buzz.io', async () => {
