dea419bebc90c36cf7e017385ddc00840823c642
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-21 22:15:38 -0400

    change oauthId type to Number

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema(
   {
-    oauthId: { type: Number, required: true, unique: true, index: true },
+    oauthId: { type: String, required: true, unique: true, index: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
