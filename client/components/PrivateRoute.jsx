fe59ee4e516c480d0f4b74f5808eea1bfd7394fd
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-26 19:23:19 -0400

    refactor PrivateRoute to render null if authenticated context is null

--- client/components/PrivateRoute.jsx
+++ client/components/PrivateRoute.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 const PrivateRoute = ({ children, ...rest }) => {
-  const user = useAuth()?.user;
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        user.authenticated ? children : <Redirect to={{ pathname: '/', state: { from: location } }} />
-      }
-    />
-  );
+  const { user } = useAuth();
+
+  return <Route {...rest} render={() => (user?.authenticated ? children : null)} />;
 };
 
 export default PrivateRoute;
