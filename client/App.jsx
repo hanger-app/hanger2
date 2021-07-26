d8f3b9455792c8c966996113a89fe2489ff6d087
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-07-26 19:37:53 -0400

    remove unused import; move Nav into Router

--- client/App.jsx
+++ client/App.jsx
@@ -3,18 +3,21 @@ import Login from './components/Login/Login.jsx';
 import Nav from './components/Nav/Nav.jsx';
 import Closet from './components/Closet/Closet.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 const App = () => {
   return (
     <div>
-      <Nav />
       <Router>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <PrivateRoute path="/closet">
-          <Closet />
-        </PrivateRoute>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <PrivateRoute path="/closet">
+            <Closet />
+          </PrivateRoute>
+        </Switch>
       </Router>
     </div>
   );
